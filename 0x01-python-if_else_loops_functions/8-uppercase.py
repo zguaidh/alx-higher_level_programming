b269510253245b9b9d9a355f980d0f87c59d362b
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-06 15:07:01 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upp = []
+    for char in str:
+        if char >= 'a' and char <= 'z':
+            val = ord(char) - 32
+            upp.append(chr(val))
+        else:
+            upp.append(char)
+    STR = ''.join(upp)
+    print(STR)
