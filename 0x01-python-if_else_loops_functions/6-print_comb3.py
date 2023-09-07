186cecdcdb9df9b6d58dcac6c74f19d2c53ae9b7
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-06 17:02:39 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-x = 0
-y = 0
-
-for x in range(9):
-    for y in range(x + 1, 10):
-        if x != y:
-            print(x, end="")
-            print(y, end="")
-        elif x == y:
-            pass
-        elif x != 8 or y != 9:
-            print(",", end="")
-            print(" ", end="")
+for x in range(10):
+    for y in range(x, 10):
+        if x <  y:
+            print("{:d}{:d}".format(x, y), 
+                    end="\n" if x == 8 and y == 9 else ", ")
