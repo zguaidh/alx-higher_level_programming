56db3989a18602eed3e0bf552d783461de30816c
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-09 02:46:49 -0700

    task 2 fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
-prog_n = sys.argv[0]
-args = sys.argv[1:]
-number = len(args)
+if __name__ == "__main__":
+    import sys
+    prog_n = sys.argv[0]
+    args = sys.argv[1:]
+    number = len(args)
 
-if number == 1:
-    print("{} argument:".format(number))
-    for arg in args:
-        print("{}: {}".format(number, arg))
-elif number > 1:
-    print("{} arguments:".format(number))
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
-else:
-    print("{} arguments.".format(number))
+    if number == 1:
+        print("{} argument:".format(number))
+        for arg in args:
+            print("{}: {}".format(number, arg))
+    elif number > 1:
+        print("{} arguments:".format(number))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
+    else:
+        print("{} arguments.".format(number))
