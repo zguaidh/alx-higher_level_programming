d4da5781c67456f435f7ea8aa761760ed3053f50
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-10 00:55:55 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
+
+
 def filter(module):
     all_attr = dir(module)
     user_attr = [attr for attr in all_attr if not attr.startswith("__")]
     return user_attr
+
 
 if __name__ == "__main__":
     user_attr = filter(hidden_4)
