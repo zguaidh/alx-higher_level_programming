41776e9082c83acb66af90cbb25babbab0ccf3e8
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-10 10:59:48 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for elem in matrix:
         for i in elem:
             if i != elem[-1]:
-                print("{}".format(i), end=" ")
+                print("{:d}".format(i), end=" ")
             else:
-                print("{}".format(i), end="")
+                print("{:d}".format(i), end="")
         print()
