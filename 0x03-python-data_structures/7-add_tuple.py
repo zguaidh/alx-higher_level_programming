2a4c6a197357dc3e801fa0ab12458d12803ac2db
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-10 23:35:38 -0700

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    length_a = len(tuple_a)
+    length_b = len(tuple_b)
+    if length_a == 0:
+        return tuple_b[:2]
+    elif length_b == 0:
+        return tuple_a[:2]
+    elif length_a == 1:
+        a = tuple_a[0]
+        c, d = tuple_b[:2]
+        x = a + c
+        y = d
+        tu = x, y
+        return tu
+    elif length_b == 1:
+        a, b = tuple_a[:2]
+        c = tuple_b[0]
+        x = a + c
+        y = b
+        tu = x, y
+        return tu
+    else:
+        a, b = tuple_a
+        c, d = tuple_b
+        x = a + c
+        y = d + b
+        tu = x, y
+        return tu
