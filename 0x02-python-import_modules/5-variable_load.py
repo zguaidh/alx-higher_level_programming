42df4c4b2e8e29c0567a58e99d845aa3c6d049ca
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-12 14:36:35 -0700

    task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-def _import(mod, var):
-    with open(f'{mod}.py', 'r') as src_f:
-        src_c = src_f.read()
-
-    global_dic = {}
-
-    return global_dic.get(var)
-
-
+from variable_load_5 import a
 if __name__ == "__main__":
-    value = _import(variable_load_5, a)
-
-    print(value)
+    print(a)
