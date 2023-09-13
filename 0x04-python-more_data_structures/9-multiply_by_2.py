47cc68974efd9590e4833c6379806d490e1dd0a9
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-13 16:26:18 -0700

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    for key, value in a_dictionary.items():
+        if value == None:
+            continue
+        else:
+            a_dictionary["key"] = lambda x: x * 2, value
+        
