fbc43dd91a383a15860c5a99674ef6770bbf24b8
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-13 11:21:36 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     sqr = []
-    for i in range(len(matrix)):  
-        sqr += [list(map(lambda x : x ** 2, matrix[i]))]
+    for i in range(len(matrix)):
+        sqr += [list(map(lambda x: x ** 2, matrix[i]))]
     return sqr
