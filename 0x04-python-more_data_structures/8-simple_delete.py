e26dd265a31f84ccdc76f1267be52513f23fb361
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-13 16:02:59 -0700

    task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for k in a_dictionary:
+        if k == key:
+            del a_dictionary["key"]
+        else:
+            continue
