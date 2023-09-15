a4958d0db3cabd2e5f8940268d4d868410c20dd2
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-15 13:43:32 -0700

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+def val_mul(x):
+    return x * 2
+
+
 def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
-        if value == None:
-            continue
-        else:
-            a_dictionary["key"] = lambda x: x * 2, value
-        
+        a_dictionary[key] = val_mul(value)
+    return a_dictionary
