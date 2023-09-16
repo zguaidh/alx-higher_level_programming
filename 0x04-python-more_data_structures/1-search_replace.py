c417f199b2b01ef6a041709ef225fe87556e93ca
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-16 02:19:06 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,11 +3,8 @@ def search_replace(my_list, search, replace):
     if search not in my_list:
         return my_list
     copy = my_list[:]
-    idx = copy.index(search)
-    copy.pop(idx)
-    copy.insert(idx, replace)
-    for i in range(len(copy)):
-        while copy[i] == search:
-            search_replace(copy, search, replace)
-            i += 1
+    while search in copy:
+        idx = copy.index(search)
+        copy.pop(idx)
+        copy.insert(idx, replace)
     return copy
