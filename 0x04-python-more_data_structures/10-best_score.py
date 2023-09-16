b8c989f94708ebdf376ad5529aaf53f09b97ab8c
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-16 05:07:22 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    score = []
     for key, value in a_dictionary.items():
-        if value == None:
-            return None  
+        score.append(value)
+    score.sort()
+    bn = len(score) - 1
+    for k, v in a_dictionary.items():
+        if v == score[bn]:
+            key = k
+    return key
