d0ec573629cd7dd5b25ce596e137699f3f1e7bdc
habiba zguaid <zguaidhabiba@gmail.com>
2023-09-26 15:58:15 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """defines a square by its size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    """ method that returns the current square area"""
+    def area(self):
+        return self.__size ** 2
+    """ method to retreive the size attribute"""
+    @property
+    def size(self):
+        return self.__size
+    """method to set the size attribute"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
