f3fc820cc627a441f2bcc72a3380d53266f50ef7
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-06 06:40:21 -0700

    task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,7 +10,14 @@ def print_square(size):
     """ method that prints a square with the character #.
         - size: is the size length of the square
         - Raises: an exception if size is less than 0 or size is a float and is less than 0 """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
 
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
 
 if __name__ == "__main__":
     import doctest
