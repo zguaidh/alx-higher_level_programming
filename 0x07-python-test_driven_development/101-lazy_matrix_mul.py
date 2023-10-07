d52ed676fed68f3795b1fb038a00f8acc0142ad0
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-07 09:27:46 -0700

    task 6

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for a function that multiplies 2 matrices by using the module NumPy
-    - Test cases should be the same as 100-matrix_mul but with new exception type/message
+    - Test cases should be the same as 100-matrix_mul
+    but with new exception type/message
     """
 
 
@@ -8,7 +9,8 @@ def lazy_matrix_mul(m_a, m_b):
     """method that multiplies 2 matrices by using the module NumPy
     -m_a: the first matrix
     -m_a: the second matrix """
-
+    if not isinstance(m_a, list):
+        raise TypeError("
 
 if __name__ == "__main__":
     import doctest
