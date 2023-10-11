4da4ca82a7e39cb013d512bed394e673a95304d4
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-11 04:26:42 -0700

    task 5

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Module for BaseGeometry class.'''
+"""Module for BaseGeometry class."""
 
 
 class BaseGeometry:
-    '''A BaseGeometry class.'''
+    """An empty BaseGeometry class."""
     pass
