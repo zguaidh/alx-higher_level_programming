62be9d3e8e6ee4df027284d7d0de1cf8a018333b
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-13 23:20:41 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Module for a read_file method"""
+
+
 def read_file(filename=""):
+    """ method that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as file:
         contents = file.read()
         print(contents, end="")
