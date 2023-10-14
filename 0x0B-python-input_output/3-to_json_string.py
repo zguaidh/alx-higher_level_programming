86b78d55edc70cf328f84e1c68684db6f1628a2a
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-14 06:11:28 -0700

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,5 +6,4 @@ import sys
 
 def to_json_string(my_obj):
     """ method that  returns the JSON representation of an object (string)"""
-    json_string = json.dumps(my_obj)
-    return json_string
+    return json.dumps(my_obj)
