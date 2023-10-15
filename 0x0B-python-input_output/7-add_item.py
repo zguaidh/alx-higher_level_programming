8fd3c50a325be2ae0d1f7665d08b8d1470fe002f
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-15 04:39:33 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Module for the add_item program"""
+'''Module for the add_item program'''
 
 
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
