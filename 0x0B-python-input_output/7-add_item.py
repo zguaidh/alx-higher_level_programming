0721e2ff4b94092f7471aef2ae81cfe473676eb2
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-14 23:49:08 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for the add_item program"""
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+args = list(sys.argv[1:])
+
+try:
+    ls = load_from_json_file('add_item.json')
+except Exception:
+    ls = []
+
+ls.extend(args)
+save_to_json_file(ls, 'add_item.json')
