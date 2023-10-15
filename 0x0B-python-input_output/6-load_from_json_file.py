13ce4ccf80a2f6a3bc4151cee62a5014ae240d39
habiba zguaid <zguaidhabiba@gmail.com>
2023-10-15 04:30:50 -0700

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,4 @@ import json
 def load_from_json_file(filename):
     """ function that creates an Object from a “JSON file” """
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        return json.loads(content)
+        return json.load(file)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = list(sys.argv[1:])
+arg = list(sys.argv[1:])
 
 try:
     ls = load_from_json_file('add_item.json')
