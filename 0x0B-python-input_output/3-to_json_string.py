8cb3c800ace2c66d990a329dc8973faecc60a922
zguaidh <133368586+zguaidh@users.noreply.github.com>
2023-12-20 21:31:36 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module for to_json_string function"""
-
-import json
-
-
-def to_json_string(my_obj):
-    """ method that  returns the JSON representation of an object (string)"""
-    return json.dumps(my_obj)
