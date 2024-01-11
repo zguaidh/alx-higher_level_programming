179877e19fd6bc6bbac801167dd2f29b5bc9fc3a
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-11 02:59:21 -0800

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,6 @@ class MyList(list):
     """ class that inherits from list"""
     def print_sorted(self):
         """ prints the sorted list"""
-        self.sort()
-        print(self)
+        ls = self.copy()
+        ls.sort()
+        print(ls)
