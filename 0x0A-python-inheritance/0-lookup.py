347321f63b851b75b7b41acf4acea3f0f223e6f5
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-11 02:38:55 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,6 @@
 
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object"""
+    """function that returns the list of
+    available attributes and methods of an object"""
     return dir(obj)
