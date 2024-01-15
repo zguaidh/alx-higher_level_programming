c6bb2bc3b92ff0018a12b471751e269e4d7d509b
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-15 12:14:44 -0800

    task 1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for Rectangle class that inherits from Base"""
+From models.Base import Base
+
+
+class Resctangle(Base):
+    """ Representes a rectangle"""
+    pass
