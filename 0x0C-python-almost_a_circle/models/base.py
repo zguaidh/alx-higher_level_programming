65ee4cdf5529ef8c40db5c1596d28c23f3d8aacd
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-19 13:01:58 -0800

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) ==0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             json_str = json.dumps(list_dictionaries)
