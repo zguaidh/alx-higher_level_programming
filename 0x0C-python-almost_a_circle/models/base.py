2a379d7b82afa2b508c44fca25b6c802b73ab476
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-19 13:37:59 -0800

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,6 +28,6 @@ class Base:
     def save_to_file(cls, list_objs):
         """ writes the JSON string representation of list_objs to a file"""
         if list_objs is not None:
-            list_objs =[o.to_dictionary() for o in list_objs]
+            list_objs = [o.to_dictionary() for o in list_objs]
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
