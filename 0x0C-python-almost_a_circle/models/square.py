a0002df7b72b9ba72c9a0a2543045463d6e5c046
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-19 12:30:20 -0800

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,24 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns attributes"""
+        if args:
+            self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            keys = list(kwargs.keys())
+            if (keys.count("id") == 1):
+                self.id = kwargs["id"]
+            if (keys.count("size") == 1):
+                self.size = kwargs["size"]
+            if (keys.count("x") == 1):
+                self.x = kwargs["x"]
+            if (keys.count("y") == 1):
+                self.y = kwargs["y"]
