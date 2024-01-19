4df29fb3e28ca59b32d953399f0c2bbeea2997dc
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-19 11:59:57 -0800

    task 9

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """The constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ The str representation of the square"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y} \
+- {self.width}"
