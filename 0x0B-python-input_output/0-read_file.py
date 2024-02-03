4efac5d41e71c334f43e04c1f07b218459ccf82f
habiba zguaid <zguaidhabiba@gmail.com>
2024-02-03 02:55:48 -0800

    readme

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# function that reads a text file (UTF8) and prints it to stdout
+""" function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
