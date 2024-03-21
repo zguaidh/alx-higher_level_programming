5f45db07e1be990e44216d529667f2fbabcadd62
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-21 14:15:06 -0700

    Fixed pycodestyle for Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
- lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -11,9 +12,10 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=username, passwd=password, db=db_name)
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     states = cur.fetchall()
 
     for state in states:
