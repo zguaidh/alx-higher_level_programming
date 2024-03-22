86115e77ef27b67cf2630d4a9cf5185d7e267ce2
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-22 14:27:11 -0700

    Updated task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all cities from the database hbtn_0e_4_usa """
+"""script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
@@ -14,12 +14,15 @@ if __name__ == "__main__":
                            user=username, passwd=password, db=db_name)
     cur = conn.cursor()
 
-    cur.execute("SELECT cities.name, states.name FROM cities JOIN
-                states ON cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id", (st_name,))
-    cities = cur.fetchall()
+    cur.execute("""SELECT cities.name, states.name
+                FROM cities JOIN states
+                ON cities.state_id = states.id
+                WHERE BINARY states.name=%s
+                ORDER BY cities.id""", (st_name,))
+    rows = cur.fetchall()
 
-    for city in cities:
-        print(city)
+    cities = [row[0] for row in rows]
+    print(", ".join(cities))
 
     cur.close()
     conn.close()
