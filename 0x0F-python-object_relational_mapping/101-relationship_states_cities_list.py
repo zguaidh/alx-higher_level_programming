d49c9dd59bb94acfd9a5010724eabc5ff48493d0
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-22 15:17:15 -0700

    Task 16 Updated

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-lists all States and corresponding Cities in the database hbtn_0e_101_usa.
-"""
+"""lists all States and corresponding Cities in the database hbtn_0e_101_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
