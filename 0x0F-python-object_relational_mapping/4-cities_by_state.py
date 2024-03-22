4cbf9995eb8d01a4d337106429ef702c9931dccd
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-22 14:28:45 -0700

    Updated task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that lists all cities from the database hbtn_0e_4_usa """
+"""script that lists all cities from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
