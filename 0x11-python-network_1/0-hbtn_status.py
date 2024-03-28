e6713d80b025d349b65f25f0aceaf804857398f4
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 15:08:58 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {utf8_content}')
