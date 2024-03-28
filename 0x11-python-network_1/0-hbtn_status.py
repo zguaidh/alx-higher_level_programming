32456c8900603251df51f2a926524a3e59e38716
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 15:10:47 -0700

    task 0 fixed the pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,8 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
         print('Body response:')
