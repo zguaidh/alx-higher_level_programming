d8677fee9142864fd2be7e585ed18c09787e2988
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 22:28:07 -0700

    Task 6 - Fixed the method used

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
     params = {'email': email}
-    response = requests.get(url, params=params)
+    response = requests.post(url, params=params)
     print(response.text)
