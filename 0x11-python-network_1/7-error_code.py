bce5d12345e1dbdc55759a4cae9320854482b8bb
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 22:44:53 -0700

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
     url = sys.argv[1]
     try:
         response = requests.get(url)
+        response.raise_for_status()
         print(response.text)
     except requests.exceptions.HTTPError as e:
-        if e.status_code >= 400:
-            print(f'Error code: {e.status_code}')
+        if e.response.status_code >= 400:
+            print(f'Error code: {e.response.status_code}')
         else:
             pass
