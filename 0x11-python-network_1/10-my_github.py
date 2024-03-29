ebc5551007db2f07abc91f37d37a30f32ebb2822
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-29 07:28:46 -0700

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and
+password) and uses the GitHub API to display your id
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=(username, password))
+    data = reponse.json()
+    if 'id' in data:
+        print(f"{data['id']}")
+    else:
+        print('None')
