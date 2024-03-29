21368fb4f74aa6cb2e20b397e4ea6c7b3dc33220
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 22:07:54 -0700

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+ Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('Body response:')
+    print(f'\t- type: {type(response.text)}')
+    print(f'\t- content: {response.text}')
