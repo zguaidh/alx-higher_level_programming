b25091737c87e19a8871db6e597f3a3c45155250
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-28 23:21:24 -0700

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) <= 1:
+        params = {'q': ""}
+    else:
+
+        params = {'q': sys.argv[1]}
+    response = requests.get(url, data=params)
+    data = response.json()
+    if data:
+        if isinstance(eval(data), dict):
+            for user in data:
+                print(f"[{user['id']}] {user['name']}")
+        else:
+            print('Not a valid JSON')
+
+    else:
+        print('No result')
+
