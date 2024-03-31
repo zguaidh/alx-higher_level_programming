43540ca4f60f0383acacc5456d166280de7a43d2
habiba zguaid <zguaidhabiba@gmail.com>
2024-03-31 01:31:04 -0700

    Task 6 Updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,12 +6,6 @@ def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
     else:
-        list_of_integers.sort()
-        return list_of_integers[size - 1]
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
