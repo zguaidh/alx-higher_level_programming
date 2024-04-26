9eb5cf30d1fb45d8a1994929e27309f1afa1afa3
zguaidh <zguaidhabiba@gmail.com>
2024-04-26 14:24:17 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,8 +2,8 @@
 const { argv } = require('node:process');
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
-function add(a, b) {
-  const c =  a + b;
+function add (a, b) {
+  const c = a + b;
   console.log(c);
   return c;
 }
