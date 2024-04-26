fc0ef84d2308535599af82a0c06af6c93030f865
zguaidh <zguaidhabiba@gmail.com>
2024-04-26 14:06:29 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+let size = parseInt(argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+}
+let i = 0;
+while (i < argv[2]) {
+  console.log('X'.repeat(size));
+  i++;
+}
