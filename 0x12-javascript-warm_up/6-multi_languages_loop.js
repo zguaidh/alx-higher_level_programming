d3bd15492a87105f5b714e0d7fde12e900138923
zguaidh <zguaidhabiba@gmail.com>
2024-04-26 13:46:07 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < array.length; i++) {
-	console.log(array[i]);
+  console.log(array[i]);
 }
