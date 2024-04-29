bea0f50bd79f345747363ffd2fdbf1dafdfa4540
zguaidh <zguaidhabiba@gmail.com>
2024-04-29 10:25:16 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
-      ;
+    
     } else {
       this.width = w;
       this.height = h;
