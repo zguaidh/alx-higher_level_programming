00aa53649db9f01ef808dafec36299a639374fa4
zguaidh <zguaidhabiba@gmail.com>
2024-04-29 13:23:27 -0700

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
+      console.log('X'.repeat(this.width));
     }
   }
 }
